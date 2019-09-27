bda23617a1e526a5e50e953bf03ee463fcdac7c1
Scott Payton <spayton.c@gmail.com>
2019-09-26 20:43:03 -0600

    need to finish last test

--- spec/active_record_american_gladiator_spec.rb
+++ spec/active_record_american_gladiator_spec.rb
@@ -16,13 +16,13 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "Powerball" do
-    xit "returns all items containing Powerball" do
+    it "returns all items containing Powerball" do
       Item.create(name: "Powerball Ball")
       Item.create(name: "Powerball Goal")
       Item.create(name: "Trap Door")
 
       # Changeable Start
-      items = Item.all
+      items = Item.where("name LIKE ?", "%Powerball%")
       # Changeable End
 
       expect(items.count).to eq(2)
@@ -30,7 +30,7 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "Hang Tough" do
-    xit "returns orders for 3 users in 2 queries (aka: Remove the N+1 query)" do
+    it "returns orders for 3 users in 2 queries (aka: Remove the N+1 query)" do
       diamond  = User.create(name: "Diamond")
       turbo    = User.create(name: "Turbo")
       laser    = User.create(name: "Laser")
@@ -45,7 +45,8 @@ describe "ActiveRecord American Gladiator" do
       order_amounts = []
 
       # Changeable Start
-      users = User.first(3)
+      users = User.includes(:orders).first(3)
+      #used eager loading here
       # Changeable End
 
       # Use eager loading to remove the N+1 query
@@ -62,7 +63,7 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "The Maze" do
-    xit "returns all users that have placed an order" do
+    it "returns all users that have placed an order" do
       gemini = User.create(name: "Gemini")
       sky    = User.create(name: "Sky")
       nitro  = User.create(name: "Nitro")
@@ -72,9 +73,10 @@ describe "ActiveRecord American Gladiator" do
       nitro.orders.create
 
       # Changeable Start
-      active_users = User.all.select do |user|
-        user.orders.present?
-      end
+      # active_users = User.all.select do |user|
+      #   user.orders.present?
+      # end
+      active_users = User.joins(:orders).distinct
       # Changeable End
 
       # Hint: http://guides.rubyonrails.org/active_record_querying.html#joining-tables
@@ -84,7 +86,7 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "Breakthrough and Conquer" do
-    xit "returns all orders with Footballs and Wrestling Rings" do
+    it "returns all orders with Footballs and Wrestling Rings" do
       wrestling_ring = Item.create(name: "Wrestling Ring")
       football       = Item.create(name: "Football")
       sweat          = Item.create(name: "Sweat")
@@ -105,7 +107,7 @@ describe "ActiveRecord American Gladiator" do
   end
 
   context "The Eliminator" do
-    xit "returns all orders placed 2 weeks ago" do
+    it "returns all orders placed 2 weeks ago" do
       last_week = Date.today.last_week
       two_weeks_ago = Date.today.last_week - 7.days
 
@@ -116,9 +118,10 @@ describe "ActiveRecord American Gladiator" do
       order_5 = Order.create(created_at: two_weeks_ago + 2.days)
 
       # Changeable Start
-      orders = Order.all.select do |order|
-        order.created_at >= two_weeks_ago && order.created_at <= last_week
-      end
+      # orders = Order.all.select do |order|
+      #   order.created_at >= two_weeks_ago && order.created_at <= last_week
+      # end
+      orders = Order.where(created_at: two_weeks_ago..last_week)
       # Changeable End
 
       expect(orders).to eq([order_1, order_3, order_5])
@@ -127,7 +130,7 @@ describe "ActiveRecord American Gladiator" do
 
   context "Atlasphere" do
     # This one is challenging.
-    xit "returns most popular items" do
+    it "returns most popular items" do
       scoring_pod = Item.create(name: "Scoring Pod")
       lights      = Item.create(name: "Lights")
       smoke       = Item.create(name: "Smoke")
@@ -136,7 +139,7 @@ describe "ActiveRecord American Gladiator" do
       Order.create(items: [lights, smoke, smoke])
       Order.create(items: [lights, lights, lights])
 
-      # Changeable Start
+      Changeable Start
       items_with_count = Hash.new(0)
 
       Order.all.each do |order|
@@ -154,6 +157,7 @@ describe "ActiveRecord American Gladiator" do
       most_popular_items = top_item_ids.map do |id|
         Item.find(id)
       end
+      # Order.where(created_at: two_weeks_ago..last_week)
       # Changeable Stop
 
       # Hints: http://apidock.com/rails/ActiveRecord/QueryMethods/select
